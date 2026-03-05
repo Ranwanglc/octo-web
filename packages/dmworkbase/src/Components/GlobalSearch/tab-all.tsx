24944836c0fbd6d282537901f39f3d2d0b48f3ef
yujiawei <yujiawei-1c1bbaa98d2b@users.noreply.github.com>
2026-03-05 16:04:15 +0800

    fix: 搜索弹窗「聊天」标签仅显示聊天记录，不再重复显示联系人和群组 (#25)
    「聊天」标签原本同时展示联系人、群组和消息，与「联系人」「群组」标签内容重复。
    现在「聊天」标签只展示消息搜索结果，联系人和群组仅在各自标签中显示。
    
    Fixes dmwork-org/dmworkim#40
    
    Co-authored-by: OpenClaw Bot <bot@openclaw.dev>

--- packages/dmworkbase/src/Components/GlobalSearch/tab-all.tsx
+++ packages/dmworkbase/src/Components/GlobalSearch/tab-all.tsx
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import { ReactNode } from "react";
 import Section from "./section";
-import ItemContacts from "./item-contacts";
-import ItemGroup from "./item-group";
 import ItemMessage from "./item-message";
 import WKApp from "../../App";
 import "./tab-all.css"
@@ -31,53 +29,9 @@ export default class TabAll extends Component<TabAllProps> {
 
     render(): ReactNode {
 
-        let existFriends = this.props.searchResult?.friends.length > 0
-        let existGroups = this.props.searchResult?.groups.length > 0
         let existMessages = this.props.searchResult?.messages.length > 0
 
         return <div className="wk-tab-all" onScroll={this.handleScroll}>
-
-            {
-                existFriends ? (<Section title="联系人">
-                    {
-                        this.props.searchResult.friends.map((item: any) => {
-                            return <ItemContacts
-                                key={item.channel_id}
-                                name={item.channel_name}
-                                avatar={WKApp.shared.avatarUser(item.channel_id)}
-                                onClick={() => {
-                                    if (this.props.onClick) {
-                                        this.props.onClick(item, "contacts")
-                                    }
-                                }}
-                            />
-                        })
-                    }
-                </Section>) : null
-            }
-            {
-                existGroups ? (
-                    <Section title="群组">
-                        {
-                            this.props.searchResult?.groups.map((item: any) => {
-                                if (this.props.keyword && item.channel_name.indexOf(this.props.keyword) !== -1) {
-                                    item.channel_name = item.channel_name.replace(this.props.keyword, `<mark>${this.props.keyword}</mark>`)
-                                }
-                                return <ItemGroup
-                                    key={item.channel_id}
-                                    name={item.channel_name}
-                                    avatar={WKApp.shared.avatarGroup(item.channel_id)}
-                                    onClick={() => {
-                                        if (this.props.onClick) {
-                                            this.props.onClick(item, "group")
-                                        }
-                                    }}
-                                />
-                            })
-                        }
-                    </Section>
-                ) : null
-            }
 
             {
                 existMessages ? (
